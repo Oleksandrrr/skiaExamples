92afb689d02bf3ff729ee08de4b91515b9deab60
“user” <oleksandr.yurchenko@extelasoft.com>
2024-09-16 15:34:48 +0300

    ChasingBubbles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,111 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React, { useEffect, useState } from "react";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Canvas, Circle } from "@shopify/react-native-skia";
+import { Dimensions, StyleSheet, View } from "react-native";
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  SharedValue,
+  useDerivedValue,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+
+const { height } = Dimensions.get("window");
+
+export const Dot = ({
+  index,
+  xPosition,
+  yPosition,
+}: {
+  index: number;
+  xPosition: SharedValue<number>;
+  yPosition: SharedValue<number>;
+}) => {
+  const currentRow = Math.floor(index / 12) * 30;
+  const currentColumn = Math.floor(index % 12) * 30 + 35;
+  const radius = useDerivedValue(() => {
+    const hypoteneuse = Math.hypot(
+      xPosition.value - currentColumn,
+      yPosition.value - 30 - currentRow
+    );
+
+    if (hypoteneuse <= 55 && xPosition.value !== -1) {
+      return withSpring(11, {
+        overshootClamping: true,
+      });
+    } else {
+      return withSpring(3, {
+        overshootClamping: true,
+      });
+    }
+  }, [xPosition, yPosition]);
+
+  return <Circle cx={currentColumn} cy={currentRow} color="blue" r={radius} />;
+};
 
 export default function HomeScreen() {
+  const [nums, setNums] = useState<number[]>([]);
+  const xPosition = useSharedValue(-1);
+  const yPosition = useSharedValue(-1);
+  const gesture = Gesture.Pan()
+    .onEnd(() => {
+      xPosition.value = -1;
+      yPosition.value = -1;
+    })
+    .onFinalize(() => {
+      xPosition.value = -1;
+      yPosition.value = -1;
+    })
+    .onBegin(({ x, y }) => {
+      xPosition.value = x;
+      yPosition.value = y;
+    })
+    .onChange(({ x, y }) => {
+      xPosition.value = x;
+      yPosition.value = y;
+    });
+
+  useEffect(() => {
+    const dotsForHeight = Math.round(height / 35);
+    const numsArray = Array.from(Array(12 * dotsForHeight).keys());
+    setNums(numsArray);
+    return () => {};
+  }, []);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView style={styles.container}>
+      <GestureHandlerRootView style={styles.container}>
+        <GestureDetector gesture={gesture}>
+          <Canvas style={{ height: "100%", width: "100%" }}>
+            {nums.map((dotIndex) => {
+              return (
+                <Dot
+                  key={dotIndex}
+                  index={dotIndex}
+                  xPosition={xPosition}
+                  yPosition={yPosition}
+                />
+              );
+            })}
+          </Canvas>
+        </GestureDetector>
+      </GestureHandlerRootView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignSelf: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    width: 400,
   },
 });
